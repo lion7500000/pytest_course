969ab3229e74273f7ec853075d67e5c89c737ca7
Andrii Koss <Lion7500000@gmail.com>
2023-10-11 18:35:44 -0700

    update test_basket.py

--- lesson1/test_basket.py
+++ lesson1/test_basket.py
@@ -12,7 +12,7 @@ REMOVE_BTN = (By.ID, "remove-sauce-labs-backpack")
 EMPTY_BASKET = (By.CLASS_NAME, "cart_desc_label")
 BACKPACK_ITEM = (By.ID, "item_4_title_link")
 ADD_TO_CARD_IN_PRODUCT_CARD = (By.ID,"add-to-cart-sauce-labs-backpack")
-CLASS_REMOVE = (By.CLASS_NAME, "removed_cart_item")
+# CLASS_REMOVE = (By.CLASS_NAME, "removed_cart_item")
 CART_BADGE = (By.CLASS_NAME, "shopping_cart_badge")
 
 
@@ -23,7 +23,7 @@ def test_add_dell_product_to_basket(driver, log_in):
     assert item_in_basket == "Sauce Labs Backpack"
     driver.find_element(*REMOVE_BTN).click()
     # empty basket should have class "removed_cart_item"
-    WebDriverWait(driver,10).until(EC.presence_of_element_located(CLASS_REMOVE))
+    WebDriverWait(driver,10).until(EC.invisibility_of_element(CART_BADGE))
 
 def test_add_dell_product_from_product_card_to_basket(driver, log_in):
     driver.find_element(*BACKPACK_ITEM).click()
